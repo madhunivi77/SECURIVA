ebec1d24c4bbb4bac80de96c68ab17f5419caeaf
Ben Jordan <ben254378@gmail.com>
2026-02-17 16:30:23 -0700

    feat: redesign login page and flow TG-136

--- frontend/src/pages/SignupForm.jsx
+++ frontend/src/pages/SignupForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link, useOutletContext } from "react-router-dom";
 
-export default function LoginForm() {
+export default function SignupForm() {
 
   const {onGoogleLogin, onSalesforceLogin, isAuthenticated = false} = useOutletContext();
 
@@ -39,15 +39,20 @@ export default function LoginForm() {
           Your browser does not support the video tag.
         </video>
       </div>
-      <div className="flex flex-col w-100 mx-auto md:mx-0 md:max-w-none justify-center pt-10 font-sans">
+      <form className="flex flex-col w-100 mx-auto md:mx-0 md:max-w-none justify-center pt-10 font-sans">
         <img src="full_logo.png" className="pb-5 w-50 mx-auto"/>
-        <h2 className="text-blue-900 text-2xl font-semibold">Welcome Back</h2>
+        <h2 className="text-blue-900 text-2xl font-semibold">Create your account</h2>
         <p className="mb-6 text-blue-900">
-          Enter your credentials to access your account
+          Fill in your details to get started with Securiva.
         </p>
-        <input className="input validator bg-white border-black text-black mb-3 w-full" type="email" required placeholder="Email" />
+        <input id="email" className="input validator bg-white border-black text-black mb-3 w-full" type="email" required placeholder="Email" />
+        <input id="phone" type="tel" className="input validator tabular-nums bg-white border-black text-black mb-3 w-full" required placeholder="Phone" 
+          pattern="[0-9]*" minLength="10" maxLength="10" title="Must be 10 digits" />
+        <input id="password" className="input bg-white border-black text-black mb-3 w-full" type="password" required placeholder="Password" minLength="8" 
+          pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" 
+          title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"/>
         <input className="input bg-white border-black text-black mb-3 w-full" type="password" required placeholder="Password" />
-        <p className="text-right text-blue-900">Forgot Password?</p>
+        <p className="text-blue-900"><input id="tos" type="checkbox" className="checkbox checkbox-info" required/> I agree to the Terms of Service and Privacy Policy</p>
         {/* Signin Button */}
         <button
           //onClick={}
@@ -57,11 +62,12 @@ export default function LoginForm() {
             transition: "background-color 0.2s",
             opacity: loadingGoogle ? 0.7 : 1,
           }}
+          type="submit"
         >
-          Sign In
+          Create account
         </button>
-        <p className="text-blue-900">Don't have an account? <Link to="/signup">Create an account</Link></p>
-      </div>
+        <p className="text-blue-900">Already have an account? <Link to="/login">Sign In</Link></p>
+      </form>
     </div>
   );
 }
