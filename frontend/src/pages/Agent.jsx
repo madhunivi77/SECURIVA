23505194f0ba3790afe67ea93dc53ccc79b39b99
Ben Jordan <ben254378@gmail.com>
2026-02-19 16:52:43 -0700

    feat: redesign agent capabilities page TG-129

--- frontend/src/pages/Agent.jsx
+++ frontend/src/pages/Agent.jsx
@@ -1,8 +1,8 @@
-import Card from "../components/Card";
 import "./Agent.css";
-import CenteredArrow from "../components/CenteredArrow";
-
+import { useOutletContext } from "react-router-dom";
+import { ChevronRight} from "lucide-react";
 function Agent() {
+    const {theme=theme} = useOutletContext();
     return (
         <div>
             <header className="hero section-min-height">
@@ -28,46 +28,50 @@ function Agent() {
                     </div>
                 </div>
             </header>
+            <div className="section-min-height">
+                <h2 className="text-center text-3xl pb-5" style={{color: theme.text}}>Meet Ava, Securiva's Intelligent AI Assistant</h2>
+                {/* Communication Channels */}
+                <div className="flex flex-col justify-center lg:flex-row mx-[10%]" style={{color: theme.text}}>
+                    <div className="card card-side bg-base-100 shadow-sm h-100">
+                        <figure>
+                            <img
+                            src="/agent_page/voice.png"
+                            alt="Voice" 
+                            className="w-2/3"/>
+                        </figure>
+                        <div className="card-body w-1/3">
+                            <h2 className="card-title">Voice Interface</h2>
+                            <p>Speak with a low latency, natural language voice assistant, equipped with your full business context.</p>
+                            <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Speak Now<ChevronRight /></button>
+                            </div>
+                        </div>
+                    </div>
 
-            <h1 style={{ textAlign: "center" }}>Meet AVA, SecuriVA's Intelligent AI Agent</h1>
+                    <div className="divider lg:divider-horizontal" >OR</div>
 
-            <div className="graph text-black">
+                    <div className="card card-side bg-base-100 shadow-sm h-100">
+                        <figure>
+                            <img
+                            src="/agent_page/text.png"
+                            alt="Chatbot"
+                            className="w-2/3" />
+                        </figure>
+                        <div className="card-body w-1/3">
+                            <h2 className="card-title">Chat Interface</h2>
+                            <p>Send typed queries or quickly upload additional context through a traditional chatbot interface.</p>
+                            <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Chat Now<ChevronRight /></button>
+                            </div>
+                        </div>
+                    </div>
 
-                <div className="layer">
-                    <Card image={"/agent_page/AVA_Assistant.png"} title={"AVA"} text={"SecuriVA AI Bot"} />
                 </div>
-
-                <CenteredArrow />
-
-                <div className="layer">
-                    <Card image={"/agent_page/email.png"} title={"Email"} text={"Send and Respond"} />
-                    <Card image={"/agent_page/whatsapp.png"} title={"WhatsApp"} text={"Auto-replies"} />
-                    <Card image={"/agent_page/sms.png"} title={"SMS Texting"} text={"Automated Alerts"} />
-                </div>
-                
-                <div style={{display: "flex", flexDirection: "row", justifyContent: "center", gap: "30%"}}>
-                    <CenteredArrow />
-                    <CenteredArrow />
-                    <CenteredArrow />
-                </div>
-
-                <div className="layer">
-                    <Card image={"/agent_page/call.png"} title={"Calls"} text={"Voice Assistant"} />
-                    <Card image={"/agent_page/calendar.png"} title={"Calendar"} text={"Meetings & Bookings"} />
-                    <Card image={"/agent_page/shield.png"} title={"Cyber Shield"} text={"Secure Workflows"} />
-                </div>
-
-                <CenteredArrow />
-
-                <div className="layer">
-                    <Card image={"/agent_page/integration.png"} title={"Integration Hub"} text={"Gmail • WhatsApp API • WordPress • Shopify • Salesforce • AWS • SMS"} />
-                </div>
-
-                <CenteredArrow />
-
-                <div className="layer">
-                    <Card image={"/agent_page/automation.png"} title={"SecuriVA Automation Engine"} text={"Emails • Calls • Tasks • Alerts • AI Responses • Scheduling"} />
-                </div>
+            </div>
+            <div className="mb-10">
+                <h2 className="text-center text-3xl pb-5" style={{color: theme.text}}>Bolster your producitivity with AI by supplying extensive business context</h2>
+                {/* Graph */}
+                <img className="section-max-height mx-auto" src="/agent_page/Graph.png"></img>
             </div>
         </div>
     )
