84325777775e5c1613e1efc5640b633c5382f0b7
Ben Jordan <ben254378@gmail.com>
2026-03-03 17:31:01 -0700

    fix: reformat automation grid code for outlet

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import { MessageSquarePlus, Mic, Settings, User } from "lucide-react";
+import { MessageSquarePlus, Mic, Settings, User, Workflow } from "lucide-react";
 import { Link } from "react-router-dom";
 
 
@@ -22,6 +22,13 @@ const Sidebar = () => {
             <button className=" inline-flex justify-normal w-full items-center text-black bg-gray-200 border-gray-300 gap-4 ">
                 <Mic className="w-4 h-4" />
                 Voice Agent
+            </button>
+        </Link>
+      {/* Automations Button */}
+        <Link to="/dashboard" className="m-2">
+            <button className=" inline-flex justify-normal w-full items-center text-black bg-gray-200 border-gray-300 gap-4 ">
+                <Workflow className="w-4 h-4" />
+                Automations
             </button>
         </Link>
 
